d9a13c7351e19abc8aeae7193df7b3b41503f8fe
Trystan Kaes <baby.hughee@gmail.com>
2018-02-04 17:04:11 -0700

    Added the initial project

---
+++ Main_Project/Main_Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Main_Project
+//
+//  Created by Trystan Kaes on 2/4/18.
+//  Copyright © 2018 QUESTIONEERING. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

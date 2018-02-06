451cd4ae643739e96587d60e39635dbafb05c1da
onyxOmega <william.fisher42@gmail.com>
2018-02-06 11:35:06 -0700

    Fixed some reference errors

---
+++ Main_Project/Main_Project/Controller/Main_Project-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Main_Project-Bridging-Header.h
+//  Main_Project
+//
+//  Created by William Fischer on 2/6/18.
+//  Copyright © 2018 QUESTIONEERING. All rights reserved.
+//
+
+#ifndef Main_Project_Bridging_Header_h
+#define Main_Project_Bridging_Header_h
+
+
+#endif /* Main_Project_Bridging_Header_h */
